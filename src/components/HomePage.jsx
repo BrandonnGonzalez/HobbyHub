d8de7906a9279ca90566f9d690321f1c0b92d054
Brandon <strobaee@gmail.com>
2025-12-08 17:39:20 -0500

    Creating basic homepage for users to navigate

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import  { IconBarbell } from '@tabler/icons-react'
+
+function HomePage() {
+
+    return (
+        <div>
+            <IconBarbell size={60}  />
+            <h1> Show Your Best!</h1>
+            <p> This is a website to connect with fellow powerlifters and show off your most favorite lifts to date of your lifting career!</p>
+            <button>Upload your favorite lift!</button>
+        </div>
+    );
+}
+
+export default HomePage;
