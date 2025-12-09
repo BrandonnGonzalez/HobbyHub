c35ea00cbdc09b693e03f3cc134d1d6d7b526f67
Brandon <strobaee@gmail.com>
2025-12-09 15:48:08 -0500

    Made the button in the homepage navigate to the form creation page

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,4 +1,5 @@
 import  { IconBarbell } from '@tabler/icons-react'
+import { Link } from 'react-router-dom'
 
 function HomePage() {
 
@@ -7,7 +8,9 @@ function HomePage() {
             <IconBarbell size={60}  />
             <h1> Show Your Best!</h1>
             <p> This is a website to connect with fellow powerlifters and show off your most favorite lifts to date of your lifting career!</p>
+            <Link to='/UploadPage'>
             <button>Upload your favorite lift!</button>
+            </Link>
         </div>
     );
 }
